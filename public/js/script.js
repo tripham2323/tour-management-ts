6e6016cdb420853cddd9c68dccedd7aab409ccf5
tripham2323 <tripd.24itb@vku.udn.vn>
2025-11-23 23:29:29 +0700

    feat: implement cart functionality with localStorage for adding tours

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,39 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 /* End Slider Tour Detail */
+
+// Carts
+// Kiểm tra trong localStorage đã có cart chưa
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const isExistTour = cart.findIndex((item) => item.tourId == tourId);
+
+      if (isExistTour == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity,
+        });
+      } else {
+        cart[isExistTour].quantity = cart[isExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// End Carts
